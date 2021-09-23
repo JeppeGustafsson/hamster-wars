45d1a3fec03fef50d376b2c4f6530ec54be52539
jeppeGustafsson <jesper.hansgustaf@gmail.com>
2021-09-23 23:57:37 +0200

    mior updates for security

--- src/init.js
+++ src/init.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('firebase-admin');
-const serviceAccount = require("../service-account-credentials.json");
+let serviceAccount;
+
+if (process.env.FIREBASE_KEY) {
+    serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+} else {
+    serviceAccount = require("../service-account-credentials.json");
+}
 
 fs.initializeApp({
     credential: fs.credential.cert(serviceAccount)
